9abab8ef8ce522f0534c37ac8d9ece598c9f86bc
bewakes <bewakepandey@gmail.com>
2021-06-21 09:18:55 +0545

    Make statusline better

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -1,25 +1,25 @@
 -- Modified from https://icyphox.sh/blog/nvim-lua/
 
 local mode_map = {
-    ['n'] = 'NORMAL ',
-    ['no'] = 'n·operator pending ',
-    ['v'] = 'VISUAL ',
-    ['V'] = 'V·LINE ',
-    [''] = 'V·BLOCK ',
-    ['s'] = 'SELECT ',
-    ['S'] = 's·line ',
-    [''] = 's·block ',
-    ['i'] = 'INSERT ',
-    ['R'] = 'REPLACE ',
-    ['Rv'] = 'v·replace ',
-    ['c'] = 'COMMAND ',
-    ['cv'] = 'vim ex ',
-    ['ce'] = 'ex ',
-    ['r'] = 'prompt ',
-    ['rm'] = 'more ',
-    ['r?'] = 'confirm ',
-    ['!'] = 'shell ',
-    ['t'] = 'TERMINAL '
+    ['n'] = 'NORMAL',
+    ['no'] = 'n·operator pending',
+    ['v'] = 'VISUAL',
+    ['V'] = 'V·LINE',
+    [''] = 'V·BLOCK',
+    ['s'] = 'SELECT',
+    ['S'] = 's·line',
+    [''] = 's·block',
+    ['i'] = 'INSERT',
+    ['R'] = 'REPLACE',
+    ['Rv'] = 'v·replace',
+    ['c'] = 'COMMAND',
+    ['cv'] = 'vim ex',
+    ['ce'] = 'ex',
+    ['r'] = 'prompt',
+    ['rm'] = 'more',
+    ['r?'] = 'confirm',
+    ['!'] = 'shell',
+    ['t'] = 'TERMINAL'
 }
 
 local function mode()
@@ -30,7 +30,10 @@ end
 
 vim.api.nvim_exec(
 [[
-    hi link PrimaryBlock Cursor
+    hi link InsertModeBlock InsertMode
+    hi link ModeBlock Cursor
+    hi link FileBlock Cursor
+    hi link BranchBlock Question
     hi link SecondaryBlock Question
     hi link Blanks StatusLine
     hi link FilePercentage DiffChange
@@ -38,25 +41,44 @@ vim.api.nvim_exec(
 
 local handle = io.popen('git branch | grep \\* | cut -d\' \' -f 2')
 local branch = handle:read("*a")
-branch = branch:gsub("%s+$", "")
+branch = branch:gsub("%s+$", "") -- remove trailing whitespacs
 handle:close()
 
 
-local stl = {
-    '%#PrimaryBlock#',
-    '%!mode()',
-    '%#SecondaryBlock#',
-    branch..' ',
-    '%#Blanks#',
-    '%f',
-    '%m',
-    '%=',
-    '%#SecondaryBlock#',
-    ' %l,%c ',
-    '%#PrimaryBlock#',
-    ' %{&filetype} ',
-    '%#FilePercentage#',
-    ' %P ',
-}
+local stl = function()
+    local m = mode()
+    local modeblock = ((m == "INSERT") and 'InsertModeBlock') or 'ModeBlock'
+    print(m, modeblock)
+    local branchinfo = ' ['..branch..'] '
+    if (branch == '') then
+        branchinfo = ''
+    end
+    return {
+        '%#'..modeblock..'#',
+        ' '..mode()..' ',
+        '%#BranchBlock#',
+        branchinfo,
+        '%#Blanks#',
+        ' %f',
+        '%m',
+        '%=',
+        '%#SecondaryBlock#',
+        ' %l,%c ',
+        '%#FileBlock#',
+        ' %{&filetype} ',
+        '%#FilePercentage#',
+        ' %P ',
+    }
+end
 
-vim.o.statusline = table.concat(stl)
+Statusline = function ()
+    return table.concat(stl())
+end
+
+vim.api.nvim_exec(
+[[
+augroup StatusLine
+au!
+au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline()
+augroup END
+]], false)
