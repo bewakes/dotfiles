eadd3b94a0ca6a0d2c4e21bcb84c15f1abe44f49
bewakes <bewakepandey@gmail.com>
2021-06-22 14:22:39 +0545

    Fix code_on_project script

--- myscripts/myscripts/code_on_project.sh
+++ myscripts/myscripts/code_on_project.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 read -p "Enter the project name: " projectname
-dir=$(cat ~/.bro/projects/$projectname)
 
-if [[ -z $1 ]]; then
+if [[ -z "$projectname" || "$projectname" == "" ]]; then
+    echo no projectname
     exit 0
 fi
-if [[ "$1" == "" ]]; then
-    exit 0
-fi
+
+dir=$(cat ~/.bro/projects/$projectname)
 
 if [[ "$1" == "setup" ]]; then
     # Check if haskell project
-    res=$(ls $dir | grep -E "^stack.yaml$|*.cabal$")
-    echo $res
-    if [[ $res ]]; then
+    is_haskell=$(ls $dir | grep -E "^stack.yaml$|*.cabal$")
+    if [[ $is_haskell ]]; then
         # fire up three terminals
-        cd $dir && termite --class haskell-nvim -e "bash" &
+        cd $dir && termite --class haskell-nvim &
         sleep 0.5s
 
         # Create a receptacle on the right
@@ -25,20 +23,22 @@ if [[ "$1" == "setup" ]]; then
         bspc node --presel-dir south --presel-ratio 0.75 -i
 
         # Spawn a terminal
-        cd $dir && termite --class haskell-stack -e "bash" &
+        cd $dir && termite --class haskell-stack &
         sleep 0.5s
         # Move the terminal to a receptacle
         bspc node $(bspc query -N -n focused) -n $(bspc query -N -n .leaf.\!window | head -n 1)
 
         # Spawn another
-        cd $dir && termite --class haskell-repl -e "bash" &
+        cd $dir && termite --class haskell-repl &
         sleep 0.5s
-        bspc node $(bspc query -N -n focused) -n $(bspc query -N -n .leaf.\!window)
 
         # Move to receptacle
-        exit 0
+        bspc node $(bspc query -N -n focused) -n $(bspc query -N -n .leaf.\!window)
+    else
+        cd $dir && termite &
     fi
-elif [[ "$1" == "just-terminal" ]]; then
-    cd $dir && termite -e "bash" &
+elif [[ "$1" == "" ]]; then
+    echo no action so just spawning terminal
+    cd $dir && termite &
 fi
 
